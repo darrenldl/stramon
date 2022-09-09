f86c7ec61904136171ce80fbc5f3edb612a22412
Daren Ldl <darrenldldev@gmail.com>
2022-09-09 22:53:40 +1000

    Added Path_utils.normalize_path

--- lib/path_utils.ml
+++ lib/path_utils.ml
@@ -1,4 +1,24 @@
-let abs_path_of_path ~(cwd : string) (path : string) =
-  if path.[0] = '/' then path
-  else
-    Filename.concat cwd path
+let normalize_path ~(cwd : string) (path : string) : string option =
+  let rec aux acc parts =
+    match parts with
+    | [] -> Some (List.rev acc)
+    | x :: xs ->
+      match x with
+      | "." | "" -> aux acc xs
+      | ".." -> (
+          match acc with
+          | [] -> None
+          | _ :: ys ->
+            aux ys xs
+        )
+      | _ -> aux (x :: acc) xs
+  in
+  let parts = String_utils.escaping_split ~on:'/' path in
+  let parts =
+    if path.[0] = '/' then parts
+    else
+      String_utils.escaping_split ~on:'/' cwd @ parts
+  in
+  aux [] parts
+  |> Option.map String_utils.concat_file_names
+  |> Option.map (fun s -> "/" ^ s)
