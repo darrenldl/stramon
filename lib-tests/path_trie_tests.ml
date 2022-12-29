6c0b8c6a6a15470b10c1a934aa854925515682d4
Darren Ldl <darrenldldev@gmail.com>
2022-12-30 03:40:08 +1100

    Added test for to_seq

--- lib-tests/path_trie_tests.ml
+++ lib-tests/path_trie_tests.ml
@@ -7,8 +7,8 @@ module Alco = struct
 end
 
 module Qc = struct
-  let add_path_and_value_pairs0 =
-    QCheck.Test.make ~count:10_000 ~name:"add_path_and_value_pairs0"
+  let add_find_path_and_value_pairs0 =
+    QCheck.Test.make ~count:10_000 ~name:"add_find_path_and_value_pairs0"
       abs_path_int_pairs
       (fun l ->
          QCheck.assume (List.for_all (fun (p, _) -> Option.is_some p) l);
@@ -33,8 +33,8 @@ module Qc = struct
            l
       )
 
-  let add_path_and_value_pairs1 =
-    QCheck.Test.make ~count:10_000 ~name:"add_path_and_value_pairs1"
+  let add_find_path_and_value_pairs1 =
+    QCheck.Test.make ~count:10_000 ~name:"add_find_path_and_value_pairs1"
       abs_path_int_pairs
       (fun l ->
          QCheck.assume (List.for_all (fun (p, _) -> Option.is_some p) l);
@@ -58,9 +58,72 @@ module Qc = struct
            )
            l
       )
+
+  let to_seq0 =
+    QCheck.Test.make ~count:10_000 ~name:"to_seq0"
+      abs_path_int_pairs
+      (fun l ->
+         QCheck.assume (List.for_all (fun (p, _) -> Option.is_some p) l);
+         let l = List.map (fun (p, x) -> (Option.get p, x)) l in
+         let trie_l =
+           List.fold_left (fun acc (p, x) ->
+               Stramon_lib.Path_trie.add p x acc
+             )
+             Stramon_lib.Path_trie.empty
+             l
+           |> Stramon_lib.Path_trie.to_seq
+           |> List.of_seq
+         in
+         let m =
+           List.fold_left (fun acc (p, x) ->
+               Path_map.add p x acc
+             )
+             Path_map.empty
+             l
+         in
+         List.length trie_l = List.length l
+         &&
+         List.for_all (fun (p, _x) ->
+             Path_map.find p m = List.assoc p trie_l
+           )
+           l
+      )
+
+  let to_seq1 =
+    QCheck.Test.make ~count:10_000 ~name:"to_seq1"
+      abs_path_int_pairs
+      (fun l ->
+         QCheck.assume (List.for_all (fun (p, _) -> Option.is_some p) l);
+         let l = List.map (fun (p, x) -> (Option.get p, x)) l in
+         let trie_l =
+           List.fold_left (fun acc (p, x) ->
+               Stramon_lib.Path_trie.add p x acc
+             )
+             Stramon_lib.Path_trie.empty
+             l
+           |> Stramon_lib.Path_trie.to_seq
+           |> List.of_seq
+         in
+         let m =
+           List.fold_left (fun acc (p, x) ->
+               String_map.add (Stramon_lib.Abs_path.to_string p) x acc
+             )
+             String_map.empty
+             l
+         in
+         List.length trie_l = List.length l
+         &&
+         List.for_all (fun (p, _x) ->
+             String_map.find (Stramon_lib.Abs_path.to_string p) m = List.assoc p trie_l
+           )
+           l
+      )
+
   let suite =
     [
-      add_path_and_value_pairs0;
-      add_path_and_value_pairs1;
+      add_find_path_and_value_pairs0;
+      add_find_path_and_value_pairs1;
+      to_seq0;
+      to_seq1;
     ]
 end
