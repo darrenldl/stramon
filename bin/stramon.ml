ac7c9f537e43c907f93ac24b05f5bb2890a5acb9
Darren Ldl <darrenldldev@gmail.com>
2022-12-29 13:46:20 +1100

    Added symlink handling

--- bin/stramon.ml
+++ bin/stramon.ml
@@ -7,9 +7,14 @@ let output_path = ref ""
 
 let force_output = ref false
 
+let no_link = ref false
+
+let latest_link_name = "stramon-latest.json"
+
 let speclist = Arg.[
     ("-o", Set_string output_path, "Output JSON file");
     ("-f", Set force_output, "Force overwrite of output file");
+    ("--no-link", Set no_link, Fmt.str "Disable adding/updating symlink %s" latest_link_name);
     ("--", Rest add_to_command, "");
   ]
 
@@ -44,7 +49,7 @@ let () =
       | s -> s
     in
     if Sys.file_exists output_path && not !force_output then (
-      Printf.printf "Error: File %s already exists\n" output_path;
+      Printf.eprintf "Error: File %s already exists\n" output_path;
       exit 1
     ) else (
       (
@@ -52,36 +57,57 @@ let () =
           CCIO.with_out output_path (fun _ -> ())
         with
         | _ -> (
-            Printf.printf "Error: Cannot open %s during test open\n" output_path;
+            Printf.eprintf "Error: Cannot open %s during test open\n" output_path;
             exit 1
           )
       );
       match Stramon_lib.monitor ~handlers:[] ~init_data:() command with
       | Error msg -> (
-          Printf.printf "Error: %s\n" msg;
+          Printf.eprintf "Error: %s\n" msg;
           exit 2
         )
       | Ok res -> (
-          try
-            CCIO.with_out output_path (fun oc ->
-                let stats = Stramon_lib.Monitor_result.stats res in
-                let json =
-                  `Assoc
-                    [
-                      ("stats", json_of_stats stats);
-                    ]
-                in
-                write_json oc json
+          (
+            try
+              CCIO.with_out output_path (fun oc ->
+                  let stats = Stramon_lib.Monitor_result.stats res in
+                  let json =
+                    `Assoc
+                      [
+                        ("stats", json_of_stats stats);
+                      ]
+                  in
+                  write_json oc json
+                )
+            with
+            | _ -> (
+                Printf.eprintf "Error: Failed to write to %s\n" output_path;
+                exit 1
               )
-          with
-          | _ -> (
-              Printf.printf "Error: Failed to write to %s\n" output_path;
+          );
+          if not !no_link then (
+            if Unix.has_symlink () then (
+              (try
+                 Sys.remove latest_link_name
+               with
+               | _ -> ()
+              );
+              (try
+                 Unix.symlink output_path latest_link_name
+               with
+               | _ -> (
+                   Printf.eprintf "Error: Failed to update symlink %s\n" latest_link_name;
+                 )
+              )
+            ) else (
+              Printf.eprintf "Error: Process cannot create symlink\n";
               exit 1
             )
+          )
         )
     )
   with
   | Sys.Break -> (
-      Printf.printf "Ctrl-C received\n";
+      Printf.eprintf "Ctrl-C received\n";
       exit 1
     )
