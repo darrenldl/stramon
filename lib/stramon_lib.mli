056041cb89a18fe3f0c8043a5822c9f819e8c5d5
Darren Ldl <darrenldldev@gmail.com>
2023-01-04 16:36:46 +1100

    Updating stramon-lib doc

--- lib/stramon_lib.mli
+++ lib/stramon_lib.mli
@@ -62,9 +62,11 @@ module File_utils : sig
   val kind_of_file : Abs_path.t -> Unix.file_kind option
 end
 
-(** {1 Main monitoring facilities and data types} *)
+(** {1 Main monitoring facilities} *)
 
 module Syscall : sig
+  (** Syscall data and handler *)
+
   type _open = {
     path : string;
     flags : string list;
@@ -92,14 +94,19 @@ module Syscall : sig
     | `_read of 'a -> int -> _read -> 'a
   ]
   (** A handler receives the user-defined "context",
-    * process id, and finally the syscall specific data.
-    * 
-    * The context is the data passed from one call of handler
-    * to the next.
+      process id, and finally the syscall specific data.
+
+      The context is the data passed from one call of handler
+      to the next.
+
+      All handlers of the same monitoring session (same [monitor] call)
+      share the same context.
   *)
 end
 
 module Stats : sig
+  (** Basic monitoring statistics *)
+
   type t
 
   val empty : t
@@ -110,8 +117,9 @@ module Stats : sig
 end
 
 module Monitor_result : sig
-  type 'a t
   (** Result of a monitoring session *)
+
+  type 'a t
 
   val data : 'a t -> 'a
   (** Final context of a monitoring session *)
@@ -137,14 +145,14 @@ val monitor :
   string list ->
   ('a Monitor_result.t, string) result
 (** [debug_level] affects debuging information printed to stderr, defaults to [`None].
-  *
-  * [stdin], [stdout], [stderr] are passed to [Unix.create_process],
-  * default to [Unix.stdin], [Unix.stdout], and [Unix.stderr] respectively.
-  *
-  * [init_data] defines the initial "context".
-  * It will be provided to the first handler to be invoked.
-  *
-  * If any of the handlers raises an exception,
-  * then monitoring is interrupted, and exception is captured in
-  * the returned monitor result.
+
+    [stdin], [stdout], [stderr] are passed to [Unix.create_process],
+    default to [Unix.stdin], [Unix.stdout], and [Unix.stderr] respectively.
+
+    [init_data] defines the initial "context".
+    It will be provided to the first handler to be invoked.
+
+    If any of the handlers raises an exception,
+    then monitoring is interrupted, and exception is captured in
+    the returned monitor result.
 *)
